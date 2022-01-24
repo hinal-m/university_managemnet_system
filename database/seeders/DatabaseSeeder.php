7219a953526c2a15050eda15772025f3a1fa1042
hinal_mevada <hinal57292gmail.com>
2022-01-24 17:29:32 -0500

    changes versions of packeges

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,13 +6,11 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(UniversitySeeder::class);
+        $this->call(CourseSeeder::class);
+        $this->call(SubjectSeeder::class);
+        $this->call(CollegeSeeder::class);
     }
 }
