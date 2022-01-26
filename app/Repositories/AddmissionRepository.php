90c41364cb748587039235233ef29a11e1f305a3
hinal_mevada <hinal57292gmail.com>
2022-01-26 18:27:18 -0500

    live error solve

--- app/Repositories/AddmissionRepository.php
+++ app/Repositories/AddmissionRepository.php
@@ -61,7 +61,7 @@ class AddmissionRepository implements AddmissionInterface
         // dd($student_mark);
         foreach($student_mark as $value)
         {
-            dd($value->commonSetting->marks);
+            dd($value->obtain_mark);
             $obtain_mark = $value->obtain_mark * $value->commonSetting->marks /100;
 
             $total_marks += $obtain_mark;
